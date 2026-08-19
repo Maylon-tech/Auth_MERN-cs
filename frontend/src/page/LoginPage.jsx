997b0d9e2094eb32a75f110f0806961da5962490
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-08-19 20:50:32 +0900

    manage the login page form.

--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
+import Input from '../components/Input'
+import { Lock, Mail, Loader } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+  }
+
+  
   return (
-    <div>
-      LoginPage
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden '
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-linear-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Welcome Back
+        </h2>
+
+        <form>
+          <Input />
+        </form>        
+      </div>
+    </motion.div>
   )
 }
 
